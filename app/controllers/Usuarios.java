4cc250c771ce2693d43679d7828ecc831e518eb4
Aninha1834 <meloanaclaudia959@gmail.com>
2021-06-28 17:34:18 -0300

    C´´odigo inicial

---
+++ app/controllers/Usuarios.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import java.util.List;
+
+import models.Usuario;
+import play.mvc.Controller;
+
+public class Usuarios extends Controller {
+	
+	public static void form() {
+		render();
+	}
+
+	public static void listar() {
+	  List<Usuario> usuarios = Usuario.findAll();
+	  render(usuarios);
+	}
+	
+	public static void salvar(Usuario usu) {
+		usu.save();
+		listar();
+	}
+	
+	public static void deletar(Long id) {
+	    Usuario usu = Usuario.findById(id);
+		usu.delete();
+		listar();
+	}
+	
+	public static void editar(Long id) {
+		Usuario usu = Usuario.findById(id);
+		renderTemplate("Usuarios/form.html", usu);
+	}
+	
+}
