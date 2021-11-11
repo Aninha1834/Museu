1f4de28b61e5a13198a8cee286ace5583dc66cfb
Aninha1834 <meloanaclaudia959@gmail.com>
2021-11-11 17:10:51 -0300

    alterando colecoes

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -137,9 +137,6 @@ public class Usuarios extends Controller {
 			nObjVisi.put(colecao.getId(), i);
 		}
 		
-		System.out.println(categorias.get(0));
-		System.out.print(colecoes.get(0));
-		
 		render(colecoesPermanentes, colecoesTemporarias, objetos, busca, categorias, nObjVisi);
 	
 	}
